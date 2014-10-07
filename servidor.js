aab8155370e37a36d867af547ee91dcd89afa85d
PabloPM <pa.ma.pa@hotmail.com>
2014-10-07 18:06:52 -0500

    Diseño web

---
+++ servidor.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var nunjucks = require("nunjucks");
+var bodyParser = require("body-parser");
+var app = express();
+app.listen(8080);
+console.log("servidor funcionando en el puerto 8000");
+
+app.use(bodyParser.urlencoded({
+	extended : true
+}));
+console.log("body parser configurado");
+
+app.use("/imagenes", express.static(__dirname + "/imagenes"));
+app.use("/estilos", express.static(__dirname + "/estilos"));
+app.use("/js", express.static(__dirname + "/js"));
+console.log("rutas estaticas configuradas");
+
+nunjucks.configure(__dirname + "/vistas", {
+	express : app
+});
+console.log("sistemas de templates configurado");
+
+app.get("/", function(req, res) {
+	res.render("index.html");
+	console.log("index enviado");
+});
